a11df04259649bf1b254fbb58fbc80ba53146ea8
1Mochiyuki <notsmile@protonmail.com>
2024-11-24 13:32:33 -0500

    feature: added cache for logins

--- ui/login/cmds.go
+++ ui/login/cmds.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/1Mochiyuki/gosky/config/logger"
 	"github.com/1Mochiyuki/gosky/db"
-	"github.com/bluesky-social/indigo/xrpc"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -24,43 +23,21 @@ type SaveCredentialsCmd struct {
 	appPass string
 }
 
-// func SaveCredentials(handle, appPass string) tea.Cmd {
-// 	saveErr := db.CreateSavedLogin(db.DB, handle, appPass, nil)
-// 	if saveErr != nil {
-// 		l.Error().Err(saveErr).Msg("there was an error saving the credentials")
-// 		return func() tea.Msg {
-// 			return SaveCredentialsCmd{
-// 				handle:  handle,
-// 				appPass: appPass,
-// 				err:     saveErr,
-// 			}
-// 		}
-// 	}
-// 	l.Info().Msgf("saved %s to db", handle)
-//
-// 	return func() tea.Msg {
-// 		return SaveCredentialsCmd{
-// 			handle:  handle,
-// 			appPass: appPass,
-// 			err:     nil,
-// 		}
-// 	}
-// }
-
 type Credentials struct {
-	handle  string
-	appPass string
-	auth    *xrpc.AuthInfo
+	Handle  string
+	AppPass string
 }
 
 func (c Credentials) String() string {
-	return fmt.Sprintf("Handle: %s Pass: %s", c.handle, c.appPass)
+	return fmt.Sprintf("Handle: %s Pass: %s", c.Handle, c.AppPass)
 }
 
 type AnyUserExistsMsg struct {
-	err     error
-	results []Credentials
+	Err     error
+	Results []Credentials
 }
+
+var LOGIN_CACHCE = map[string]string{}
 
 func AnyCredentialsExist() tea.Msg {
 	var rowCount int
@@ -68,39 +45,53 @@ func AnyCredentialsExist() tea.Msg {
 	if err != nil {
 		l.Error().Err(err).Msg("error checking any creds exist")
 		return AnyUserExistsMsg{
-			err: err,
+			Err: err,
 		}
 	}
+
+	var results []Credentials
+	var handle, pass string
+
 	if rowCount == 0 {
 		l.Warn().Msg("db is empty")
 		return AnyUserExistsMsg{
-			err: err,
+			Err: err,
+		}
+	}
+	if rowCount == len(LOGIN_CACHCE) {
+		l.Debug().Msg("pulling from cache")
+		for handle, pass := range LOGIN_CACHCE {
+			results = append(results, Credentials{Handle: handle, AppPass: pass})
+		}
+		return AnyUserExistsMsg{
+			Results: results,
+			Err:     nil,
 		}
 	}
 	rows, err := db.DB.Query("SELECT * FROM users")
 	if err != nil {
 		l.Error().Err(err).Msg("error selecting all users from table")
 		return AnyUserExistsMsg{
-			err: err,
+			Err: err,
 		}
 	}
 	defer rows.Close()
-	var results []Credentials
-	var handle, pass string
 	for rows.Next() {
 		err = rows.Scan(&handle, &pass)
 		if err != nil {
 			l.Fatal().Err(err).Msg("error retrieving details from db")
 			return AnyUserExistsMsg{
-				err: err,
+				Err: err,
 			}
 		}
-		results = append(results, Credentials{handle: handle, appPass: pass})
+		LOGIN_CACHCE[handle] = pass
+		results = append(results, Credentials{Handle: handle, AppPass: pass})
 	}
 	if err = rows.Err(); err != nil {
 		l.Fatal().Err(err).Msg("an error occurred getting next row")
 	}
+	l.Debug().Msg("pulled from db, added to cache")
 	return AnyUserExistsMsg{
-		results: results,
+		Results: results,
 	}
 }
